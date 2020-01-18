b786d71d3a4818079059c6ae9cc42678193798b7
kevinreber <kevinreber1@gmail.com>
2020-01-17 18:06:20 -0800

    tweaked style and match cards feature

--- app.js
+++ app.js
@@ -1,46 +1,82 @@
 const overlay = document.getElementById('overlay');
 const startBtn = document.querySelector('.start-btn');
+const counter = document.getElementById('counter');
 const container = document.getElementById('container');
 const cards = document.querySelectorAll('.card');
 const imageData = [
-    'images/bikes.jpg', 
-    'images/bridge.jpg', 
-    'images/exit.jpg', 
-    'images/leaves.jpg', 
-    'images/leaves_on_ground.jpg', 
-    'images/scooters.jpg'
+    'images/cardano-ada-logo.png',
+    'images/eos-eos-logo.png',
+    'images/ethereum-eth-logo.png',
+    'images/litecoin-ltc-logo.png',
+    'images/monero-xmr-logo.png',
+    'images/neo-neo-logo.png',
+    'images/stellar-xlm-logo.png'
 ];
-let card1;
-let card2;
+let card1, card2;
+// let lockBoard = false;
+let attemptToMatch = false;
 let count = 0;
 
 // function randomNum(){
 //     return Math.floor(Math.random() * 12);
 // }
 
-function startGame(){
+function startGame() {
     overlay.style.display = 'none';
 }
 
-function flipCard(){
-//console.log(this.classList.add('flip'));
-this.classList.toggle('flip');
-    // e.target.classList.add('active');
-    // if(card1){
-    //     card2 = e.target.getAttribute('src');
-    // }  else{
-    //     card1 = e.target.getAttribute('src');
-    // }
+function updateCount() {
+    count++;
+    counter.querySelector('h3').textContent = count;
+}
 
-    if(card1 && card2){
-        //checkMatch();
+function flipCard() {
+    if (this === card1) return;
+    else updateCount();
+    this.classList.add('flip');
+    this.classList.add('active');
+
+    if (!attemptToMatch) {
+        attemptToMatch = true;
+        card1 = this;
+        return; //Need return to store card1 and end function
     }
-    
-    
+    card2 = this;
+    checkMatch();
+}
+
+function checkMatch() {
+    return card1.dataset.crypto === card2.dataset.crypto ? cardsMatch() : cardsDoNotMatch();
+}
+
+function cardsMatch() {
+    cardMatchProp(card1);
+    cardMatchProp(card2);
+    resetBoard();
+}
+
+function cardMatchProp(card) {
+    card.classList.remove('active');
+    card.classList.add('match');
+    card.removeEventListener('click', flipCard);
+}
+
+function cardsDoNotMatch() {
+    setTimeout(() => {
+        card1.classList.remove('flip');
+        card2.classList.remove('flip');
+        resetBoard();
+    }, 2000);
+}
+
+function resetBoard() {
+    card1 = null;
+    card2 = null;
+    attemptToMatch = false;
 }
 
 //startBtn.addEventListener('click', startGame);
 
-for (let card of cards){
+for (let card of cards) {
     card.addEventListener('click', flipCard);
 }
