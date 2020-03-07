dc08cda299a42d021a3425ca81f73558d6f761a7
kevinreber <kevinreber1@gmail.com>
2020-03-07 00:30:41 -0800

    refactored code

--- app.js
+++ app.js
@@ -1,217 +1,247 @@
-const overlay = document.getElementById('overlay');
-const gameBtn = document.getElementById('game-button');
-let highScore = 0;
-let card1, card2;
-let lockBoard = true;
-let attemptToMatch = false;
-let matches = 0;
-let count = 0;
+let game;
 
-//Random number to shuffle card order
-function randomNum() {
-    return Math.floor(Math.random() * 17);
-}
+//Start new game when window loads
+window.addEventListener('load', () => {
+    game = new MemoryGame();
+});
 
-//Checks localStorage for highScore
-function checkHighScore() {
-    let bestScore = document.getElementById('best-score');
-    if (localStorage.highScore) {
-        highScore = JSON.parse(localStorage.highScore);
-        bestScore.textContent = highScore; //Update Best Score
-    } else bestScore.textContent = '-';
-    renderGameBoard();
-}
+class MemoryGame {
+    constructor() {
+        // DOM
+        this.overlay = document.getElementById('overlay');
+        this.gameBtn = document.getElementById('game-button');
 
-//Renders cards
-function renderCard(coin) {
-    //Data on cryptos and their source
-    const cryptos = {
-        ada: {
-            data: "ada",
-            src: "cardano-ada-logo.png"
-        },
-        eos: {
-            data: "eos",
-            src: "eos-eos-logo.png"
-        },
-        eth: {
-            data: "eth",
-            src: "ethereum-eth-logo.png"
-        },
-        trx: {
-            data: "trx",
-            src: "tron-trx-logo.png"
-        },
-        ltc: {
-            data: "ltc",
-            src: "litecoin-ltc-logo.png"
-        },
-        neo: {
-            data: "neo",
-            src: "neo-neo-logo.png"
-        },
-        xmr: {
-            data: "xmr",
-            src: "monero-xmr-logo.png"
-        },
-        xlm: {
-            data: "xlm",
-            src: "stellar-xlm-logo.png"
+        // Game Data
+        this.highScore = 0;
+        this.card1;
+        this.card2;
+        this.lockBoard = true;
+        this.attemptToMatch = false;
+        this.matches = 0;
+        this.count = 0;
+
+        // Render board game and add Event listeners
+        this.renderGameBoard();
+        this.addEvents();
+    }
+
+    //Renders gameBoard with cards
+    renderGameBoard() {
+        const gameBoard = document.getElementById('gameboard');
+        gameBoard.innerHTML = `
+        ${new Card("ada").buildCard()}
+        ${new Card("eos").buildCard()}
+        ${new Card("ada").buildCard()}
+        ${new Card("eth").buildCard()}
+        ${new Card("trx").buildCard()}
+        ${new Card("ltc").buildCard()}
+        ${new Card("neo").buildCard()}
+        ${new Card("xmr").buildCard()}
+        ${new Card("xlm").buildCard()}
+        ${new Card("neo").buildCard()}
+        ${new Card("eth").buildCard()}
+        ${new Card("xmr").buildCard()}
+        ${new Card("trx").buildCard()}
+        ${new Card("xlm").buildCard()}
+        ${new Card("eos").buildCard()}
+        ${new Card("ltc").buildCard()}
+    `;
+    }
+
+    addEvents() {
+        this.gameBtn.addEventListener('click', this.startGame);
+    }
+
+    startGame() {
+        console.log('start');
+
+        let gameScore = document.getElementById('game-score');
+        this.checkHighScore();
+        // this.overlay.style.display = 'none';
+        this.count = 0;
+        // if (this.gameBtn.textContent = 'RESTART') {
+        //     this.resetGame();
+        // }
+        // gameScore.textContent = this.count; //Reset score
+        // this.lockBoard = false; //Allow user to click cards
+        // this.gameBtn.textContent = 'RESTART'; //Restart button
+        // this.gameBtn.style.opacity = '.6';
+        this.shuffleCards();
+    }
+
+    //Checks localStorage for highScore
+    checkHighScore() {
+        // let bestScore = document.getElementById('best-score');
+        // if (localStorage.highScore) {
+        //     this.highScore = JSON.parse(localStorage.highScore);
+        //     bestScore.textContent = this.highScore; //Update Best Score
+        // } else bestScore.textContent = '-';
+    }
+
+    //Random number to shuffle card order
+    randomNum() {
+        return Math.floor(Math.random() * 17);
+    }
+
+    //Shuffle order of cards when player starts game
+    shuffleCards() {
+        const cards = document.querySelectorAll('.card');
+        for (let card of cards) {
+            card.classList.remove('flip', 'match');
+            card.style.opacity = '1';
+            card.style.order = this.randomNum();
+            card.addEventListener('click', this.flipCard);
         }
-    };
-    const crypto = cryptos[coin];
-    let html = `
-    <div class="card" data-crypto="${crypto.data}">
-        <img class="card-face front" src="images/${crypto.src}" alt="image">
-        <img class="card-face back" src="images/cover/bitcoin-btc-logo.png" alt="cover">
-    </div>
-    `;
-    return html;
-}
-
-//Renders gameBoard with cards
-function renderGameBoard() {
-    const gameBoard = document.getElementById('gameboard');
-    gameBoard.innerHTML = `
-        ${renderCard("ada")}
-        ${renderCard("eos")}
-        ${renderCard("ada")}
-        ${renderCard("eth")}
-        ${renderCard("trx")}
-        ${renderCard("ltc")}
-        ${renderCard("neo")}
-        ${renderCard("xmr")}
-        ${renderCard("xlm")}
-        ${renderCard("neo")}
-        ${renderCard("eth")}
-        ${renderCard("xmr")}
-        ${renderCard("trx")}
-        ${renderCard("xlm")}
-        ${renderCard("eos")}
-        ${renderCard("ltc")}
-    `;
-}
-
-function startGame() {
-    let gameScore = document.getElementById('game-score');
-    checkHighScore();
-    overlay.style.display = 'none';
-    count = 0;
-    if (gameBtn.textContent = 'RESTART') {
-        resetGame();
     }
-    gameScore.textContent = count; //Reset score
-    lockBoard = false; //Allow user to click cards
-    gameBtn.textContent = 'RESTART'; //Restart button
-    gameBtn.style.opacity = '.6';
-    shuffleCards();
-}
 
-//Reset game shuffles cards
-function resetGame() {
-    matches = 0;
-    count = 0;
-    resetCards();
-    shuffleCards();
-}
-
-//Shuffle order of cards when player starts game
-function shuffleCards() {
-    const cards = document.querySelectorAll('.card');
-    for (let card of cards) {
-        card.classList.remove('flip', 'match');
-        card.style.opacity = '1';
-        card.style.order = randomNum();
-        card.addEventListener('click', flipCard);
+    //Reset game shuffles cards
+    resetGame() {
+        this.matches = 0;
+        this.count = 0;
+        this.resetCards();
+        this.shuffleCards();
     }
-}
 
-//Updates how many guess player has made
-function updateCount() {
-    let gameScore = document.getElementById('game-score');
-    count++;
-    gameScore.textContent = count;
-}
-
-//Flips card  when player has made a guess
-function flipCard() {
-    if (lockBoard || this === card1) return; //Locks card1 so player cannot flip card1 again
-    else updateCount();
-    this.classList.add('flip');
-
-    if (!attemptToMatch) {
-        attemptToMatch = true;
-        card1 = this;
-        return; //Need return to store card1 and end function
+    resetCards() {
+        this.card1 = null;
+        this.card2 = null;
+        this.attemptToMatch = false;
+        this.lockBoard = false;
     }
-    card2 = this;
-    checkMatch(); //Check if cards match when player has guessed card2
-}
 
-//Checks if cards match
-function checkMatch() {
-    return card1.dataset.crypto === card2.dataset.crypto ? cardsMatch() : cardsDoNotMatch();
-}
+    //Updates how many guess player has made
+    updateCount() {
+        let gameScore = document.getElementById('game-score');
+        this.count++;
+        gameScore.textContent = this.count;
+    }
 
-//If cards match, card1 and card2 remain face up
-function cardsMatch() {
-    matches++;
-    if (matches === 8) endGame(); //If all cards match, end game
-    cardMatchProp(card1);
-    cardMatchProp(card2);
-    resetCards();
-}
+    //Flips card  when player has made a guess
+    // TODO: may need to change 'this' to 'e.target'
+    flipCard() {
+        if (this.lockBoard || this === this.card1) return; //Locks card1 so player cannot flip card1 again
+        else this.updateCount();
+        this.classList.add('flip');
 
-//Removes player being able to select cards that are matched
-function cardMatchProp(card) {
-    card.removeEventListener('click', flipCard);
-}
+        if (!this.attemptToMatch) {
+            this.attemptToMatch = true;
+            this.card1 = this;
+            return; //Need return to store card1 and end function
+        }
+        this.card2 = this;
+        this.checkMatch(); //Check if cards match when player has guessed card2
+    }
 
-//If cards don't match, cards are flipped back to face down
-function cardsDoNotMatch() {
-    lockBoard = true; //to prevent user from clicking cards before board resets
-    setTimeout(() => {
-        card1.classList.remove('flip');
-        card2.classList.remove('flip');
-        resetCards();
-    }, 1000);
-}
+    //Checks if cards match
+    checkMatch() {
+        return this.card1.dataset.crypto === this.card2.dataset.crypto ? this.cardsMatch() : this.cardsDoNotMatch();
+    }
 
-function resetCards() {
-    card1 = null;
-    card2 = null;
-    attemptToMatch = false;
-    lockBoard = false;
-}
+    //If cards match, card1 and card2 remain face up
+    cardsMatch() {
+        this.matches++;
+        if (this.matches === 8) this.endGame(); //If all cards match, end game
+        this.cardMatchProp(this.card1);
+        this.cardMatchProp(this.card2);
+        this.resetCards();
+    }
 
-//End game screen and checks if player has beat previous highScore
-function endGame() {
-    let html;
-    if (count < highScore || !localStorage.highScore) { //Compare count to highScore and checks if local.Storage.highScore exists
-        highScore = count;
-        localStorage.setItem('highScore', JSON.stringify(highScore)); //Update local storage
-        html = `
+    //Removes player being able to select cards that are matched
+    cardMatchProp(card) {
+        card.removeEventListener('click', this.flipCard);
+    }
+
+    //If cards don't match, cards are flipped back to face down
+    cardsDoNotMatch() {
+        this.lockBoard = true; //to prevent user from clicking cards before board resets
+        setTimeout(() => {
+            this.card1.classList.remove('flip');
+            this.card2.classList.remove('flip');
+            this.resetCards();
+        }, 1000);
+    }
+
+
+
+    //End game screen and checks if player has beat previous highScore
+    endGame() {
+        let html;
+        if (this.count < this.highScore || !localStorage.highScore) { //Compare count to highScore and checks if local.Storage.highScore exists
+            this.highScore = this.count;
+            localStorage.setItem('highScore', JSON.stringify(this.highScore)); //Update local storage
+            html = `
             <div class="overlay-content">
                 <h1>New Best Score!</h1>
-                <h3>${count}</h3>
+                <h3>${this.count}</h3>
                 <a href="#" id="new-game-button" class="btn">PLAY AGAIN</a>
             </div>
         `
-    } else {
-        html = `
+        } else {
+            html = `
             <div class="overlay-content">
                 <h1>You Win!</h1>
-                <h3>${count}</h3>
+                <h3>${this.count}</h3>
                 <a href="#" id="new-game-button" class="btn">PLAY AGAIN</a>
             </div>
             `
-    }
-    overlay.innerHTML = html;
-    overlay.style.display = 'block';
+        }
+        this.overlay.innerHTML = html;
+        this.overlay.style.display = 'block';
 
-    document.getElementById('new-game-button').addEventListener('click', startGame);
+        document.getElementById('new-game-button').addEventListener('click', this.startGame);
+    }
+
 }
 
-//EVENT LISTENERS
-window.onload = checkHighScore();
-gameBtn.addEventListener('click', startGame);
+class Card {
+    constructor(coin) {
+        //Data on cryptos and their source
+        const cryptos = {
+            ada: {
+                data: "ada",
+                src: "cardano-ada-logo.png"
+            },
+            eos: {
+                data: "eos",
+                src: "eos-eos-logo.png"
+            },
+            eth: {
+                data: "eth",
+                src: "ethereum-eth-logo.png"
+            },
+            trx: {
+                data: "trx",
+                src: "tron-trx-logo.png"
+            },
+            ltc: {
+                data: "ltc",
+                src: "litecoin-ltc-logo.png"
+            },
+            neo: {
+                data: "neo",
+                src: "neo-neo-logo.png"
+            },
+            xmr: {
+                data: "xmr",
+                src: "monero-xmr-logo.png"
+            },
+            xlm: {
+                data: "xlm",
+                src: "stellar-xlm-logo.png"
+            }
+        };
+
+        this.crypto = cryptos[coin];
+    }
+
+    //Renders cards
+    buildCard() {
+        let html = `
+        <div class="card" data-crypto="${this.crypto.data}">
+            <img class="card-face front" src="images/${this.crypto.src}" alt="image">
+            <img class="card-face back" src="images/cover/bitcoin-btc-logo.png" alt="cover">
+        </div>
+        `;
+        return html;
+    }
+}
